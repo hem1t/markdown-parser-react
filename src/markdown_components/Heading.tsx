f0cd3a6dea71cd16ebb056b3d75f453b740c3643
hem1t <sunny10fb@gmail.com>
2024-06-18 08:56:26 +0530

    h needs custom id

--- src/markdown_components/Heading.tsx
+++ src/markdown_components/Heading.tsx
@@ -3,25 +3,51 @@ import { useThemeContext } from "../themes/themes";
 interface Prop {
   level: number;
   text: string;
+  id?: string;
 }
 
-export const Heading = ({ level, text }: Prop) => {
+export const Heading = ({ level, text, id }: Prop) => {
   let theme = useThemeContext();
+  id = id ? id : text.replace(" ", "_").toLocaleLowerCase();
 
   level = Math.min(6, Math.max(level, 0));
   switch (level) {
     case 1:
-      return <h1 className={theme.h1 + " leading-10"}>{text}</h1>;
+      return (
+        <h1 id={id} className={theme.h1 + " leading-10"}>
+          {text}
+        </h1>
+      );
     case 2:
-      return <h2 className={theme.h2 + " leading-10"}>{text}</h2>;
+      return (
+        <h2 id={id} className={theme.h2 + " leading-10"}>
+          {text}
+        </h2>
+      );
     case 3:
-      return <h3 className={theme.h3 + " leading-10"}>{text}</h3>;
+      return (
+        <h3 id={id} className={theme.h3 + " leading-10"}>
+          {text}
+        </h3>
+      );
     case 4:
-      return <h4 className={theme.h4 + " leading-10"}>{text}</h4>;
+      return (
+        <h4 id={id} className={theme.h4 + " leading-10"}>
+          {text}
+        </h4>
+      );
     case 5:
-      return <h5 className={theme.h5 + " leading-10"}>{text}</h5>;
+      return (
+        <h5 id={id} className={theme.h5 + " leading-10"}>
+          {text}
+        </h5>
+      );
     case 6:
-      return <h6 className={theme.h6 + " leading-10"}>{text}</h6>;
+      return (
+        <h6 id={id} className={theme.h6 + " leading-10"}>
+          {text}
+        </h6>
+      );
     default:
       throw Error("heading level not right, somehow!");
   }
